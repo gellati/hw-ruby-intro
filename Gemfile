e4fb7a0324a38792093f7a51c70a27c23d048dfc
Omar El-Begawy <omar.elbegawy@gmail.com>
2018-03-13 08:52:14 +0200

    initial solutions

--- Gemfile
+++ Gemfile
@@ -1,4 +1,13 @@
 source 'https://rubygems.org'
-ruby '2.3.0'
-
+# ruby '2.3'
+ruby '2.3.4'
 gem 'rspec'
+gem 'rake'
+
+
+group :development do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-shell'
+end
+
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # When done, submit this entire file to the autograder.
 
 # Part 1
@@ -5,37 +6,84 @@
 def sum arr
   if arr.empty? then return 0 end
   arr.reduce(:+)
+
+#  if arr.empty?
+#    0
+#  else
+#    arr.reduce(:+)
+#  end
+
 end
+
+
 
 def max_2_sum arr
 #  sum arr unless arr.length > 2
   if arr.empty? then return 0 end
   if arr.length < 2 then sum arr end
   arr.sort{|x,y| y <=> x}.take(2).reduce(:+)
+
+#  if arr.empty?
+#    0
+#  elsif arr.length == 1
+#    arr.last
+#  else
+#    arr.max(2){|a,b| a <=> b}.reduce(:+)
+#  end
+
 end
 
 def sum_to_n? arr, n
   puts arr.combination(2).to_a.uniq{|s| s.first}
   arr.combination(2).to_a.uniq.flat_map{|x| return x == n ? true : false }
-  # YOUR CODE HERE
+
+#  if arr.length <= 1
+#    false
+#  else
+#    arr.combination(2).to_a.map{|x| x.reduce(:+) }.include?(n) # then true else end}.include?(true)
+#  end
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
+# http://www.rexegg.com/regex-class-operations.html
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[A-Za-z&&[^aeiouAEIOU]]/
+    true
+  else
+    false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[01]+$/
+    if (s.to_i(2) % 4) == 0
+      true
+    else
+      false
+    end
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new('ISBN should not be an empty string') if isbn.to_s.empty?
+    raise ArgumentError.new('Price should not be negative') if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  attr_accessor :isbn, :price
+
+  def price_as_string
+    p = "$%.2f" % @price
+  end
 end
